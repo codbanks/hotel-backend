0dbaa786fe2d1d4ece85f6e3fc286921f41b59d2
codbanks <tozvirevatapiwanashe@gmail.com>
2026-01-23 12:12:33 +0200

    Smplify asgi.py to fix module not found error

--- config/asgi.py
+++ config/asgi.py
@@ -1,30 +1,12 @@
-"""
-ASGI config for config project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
-"""
-
 import os
-
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
+
+# 1. Set the settings
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-from channels.auth import AuthMiddlewareStack
-import hotel_app.routing
-application = get_asgi_application()
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
-
+# 2. Get the ASGI application (this handles your urls.py automatically)
 django_asgi_app = get_asgi_application()
 
-application = ProtocolTypeRouter({
-    "http": django_asgi_app,
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            hotel_app.routing.websocket_urlpatterns
-        )
-    ),
-})
+# 3. Simple application export
+# This tells Render: "Just handle HTTP requests using my standard URLs"
+application = django_asgi_app
